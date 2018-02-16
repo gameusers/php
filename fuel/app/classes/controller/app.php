c5af29ff21d8ceac97b4450526e03c847fe00991
Kazuki Azumi <rodinia@hotmail.co.jp>
2018-02-16 18:59:36 +0900

    シェアボタン更新 - 2018/2/16

--- fuel/app/classes/controller/app.php
+++ fuel/app/classes/controller/app.php
@@ -288,8 +288,8 @@ class Controller_App extends Controller
 		//    root-bundle.min.js
 		// ---------------------------------------------
 
-		array_push($jsArr, ['src' => URL_BASE . 'react/js/root-bundle.min.js?ver=1.1.9.0']);
-        array_push($jsArr, ['src' => URL_BASE . 'react/lib/game-users-share-buttons/js/share-bundle.min.js?ver=1.2.1.0']);
+		array_push($jsArr, ['src' => URL_BASE . 'react/js/root-bundle.min.js?ver=1.2.0.0']);
+        array_push($jsArr, ['src' => URL_BASE . 'react/lib/game-users-share-buttons/js/share-bundle.min.js?ver=1.3.0.0']);
 
 
 
